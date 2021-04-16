a3e4ef166ae5825485b5433ddbe545df4ed4cc18
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-04-16 08:25:50 -0400

    2021

--- CompareCalibrated.py
+++ CompareCalibrated.py
@@ -16,10 +16,11 @@ calibratedModelWithDrug = Cls.CalibratedModel(
     csv_file_name=CALIBRATION_ROOT_DIR+'\CalibrationResults.csv',
     drug_effectiveness_ratio=D.DRUG_EFFECT_RATIO)
 # simulate the calibrated model
+initial_seed = D.NUM_SIM_COHORTS * D.SIM_POP_SIZE
 calibratedModelWithDrug.simulate(num_of_simulated_cohorts=D.NUM_SIM_COHORTS,
                                  cohort_size=D.SIM_POP_SIZE,
                                  time_steps=D.TIME_STEPS,
-                                 cohort_ids=range(5000, 5000 + D.NUM_SIM_COHORTS))
+                                 cohort_ids=range(initial_seed, initial_seed + D.NUM_SIM_COHORTS))
 
 # report mean and projection interval of expected survival time
 Support.print_outcomes(calibrated_model=calibratedModelNoDrug,
